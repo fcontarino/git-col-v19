2a68a4251546c10c757455a828c94b660ead9609
fcontarino <facontarino@gmail.com>
2021-09-24 22:09:49 -0300

    Logica Hamburguesa

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Declaracion
+const d = document
+const menubtn = d.querySelector('nav .btn')
+const manuList = d.querySelector('nav .menu')
+
+// funciones
+menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
